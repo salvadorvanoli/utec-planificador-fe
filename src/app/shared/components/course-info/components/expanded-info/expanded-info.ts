34d93fa4a130aac3dd3f4b232d5eb7ad8a8266e5
Valentín Veintemilla Marchisio <valentinveintemilla@gmail.com>
2025-11-20 05:28:28 -0300

    Correct import error in expanded-info

--- src/app/shared/components/course-info/components/expanded-info/expanded-info.ts
+++ src/app/shared/components/course-info/components/expanded-info/expanded-info.ts
@@ -3,7 +3,7 @@ import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabel } from 'primeng/floatlabel';
 import { FormsModule } from '@angular/forms';
-import { TagsBox } from '../course-info/components/tags-box/tags-box';
+import { TagsBox } from '../tags-box/tags-box';
 import { Course } from '@app/core/models';
 import { EnumResponse } from '@app/core/services';
 
